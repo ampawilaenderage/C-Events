6c78d7d6da5c34b445dff0dbcea3f86895553cd0
Sajith <ampawilaenderage@gmail.com>
2026-03-01 15:48:25 +0100

    test push

--- Event.cs
+++ Event.cs
@@ -75,7 +75,7 @@ class Zufallszahlengenerator
         return zufallszahl;
 
     }
-  // git pull test 2
+  // git push test 
     public class ZahlEventArgs : EventArgs
     {
         public int Zahl { get; }
